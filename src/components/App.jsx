40d26a9fda11b6733a287b595bc0a5005af2ae25
gaynor-landmark <g.l.landmark@gmail.com>
2016-05-03 19:47:37 +1200

    App.js added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,35 @@
-<h3 class='row spacer'>
-  <label>
-    Value :
-  </label>
-  <div id=val>
-  </div>
-</h3>
-<div class='row spacer'>
-  <button>+ 1</button>
-  <button>- 1</button>
-</div>
+'use strict'
+import React from 'react'
+
+module.exports = React.createClass({
+
+  getInitialState: function () {
+
+    console.log("init")
+
+    return {
+      val: 0
+    }
+  },
+
+  render: function () {
+    console.log("render app", this.state, this.state.val)
+    return (
+      <div>
+        <h3 className='row spacer'>
+
+            Value :
+        
+
+            {this.state.val}
+
+        </h3>
+        <div className='row spacer'>
+          <button>+ 1</button>
+          <button>- 1</button>
+        </div>
+      </div>
+    )
+  }
+
+})
